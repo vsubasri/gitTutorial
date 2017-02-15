7e08aa01b3abf9652beb74cbf9c09fb582a1bbaa
Valli Subasri <vsubasri@uwo.ca>
2017-02-15 16:15:29 -0500

    git and GitHub tutorial -- merge additions

--- src/Student.java
+++ src/Student.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class Student extends Person {
     String major;
     List<Course> courseList;
+    boolean fulltime;
 
-    public Student(String name, String emailAddress, String major) {
+    public Student(String name, String emailAddress, String major, Boolean fulltime) {
         super(name, emailAddress);
         this.major = major;
+        this.fulltime = fulltime;
     }
 
     public String toString() {
